766247070ac0298abcece7cb6a3fd72d791c5cf2
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-01-30 21:24:06 -0600

    Adjust schema

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20160131022334) do
 
   create_table "tournaments", force: :cascade do |t|
     t.string   "name"
-    t.string   "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
