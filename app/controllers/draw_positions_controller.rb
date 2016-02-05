7c207911e3bb782762044a2de0e53eefaf5d74a0
pdgonzalez872 <pdgonzalez872@gmail.com>
2016-02-04 22:39:17 -0600

    Add Winner/Loser functionality

--- app/controllers/draw_positions_controller.rb
+++ app/controllers/draw_positions_controller.rb
@@ -3,21 +3,25 @@ class DrawPositionsController < ApplicationController
   def edit
     @dp = DrawPosition.find_by(id: params[:id])
     @player_options = Draw.fetch_player_options(@dp)
-    # @match = Match.where(draw_positions: @dp)
   end
 
   def update
+    # There is a mathematical relationship where the draw_position id
+    # is the same as the Match number. I use this to update both
+    # the draw position and also the match.
     dp = DrawPosition.find_by(id: params[:id])
     player = Player.find_by(name: params[:draw_position][:winner])
-    match = Match.find_by(id: dp.match.id)
+
+    previous_match = Match.find_by(match_number: params[:id])
+    previous_match.winner=(player.id)
+    previous_match.calculate_loser
+    previous_match.save
 
     if dp.players << player
       redirect_to tournament_draw_path(tournament_id: dp.draw.tournament.id,
-                                       id: dp.draw.id)
+                                                  id: dp.draw.id)
     else
       render "edit"
     end
-    # find the player with the name that is passed, thenif save redirect
   end
-
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,22 @@
 class Match < ActiveRecord::Base
-  has_many   :draw_positions
+  has_many :draw_positions
   has_many :players, through: :draw_positions
 
   belongs_to :location
   belongs_to :draw
+
+  def winner=(player_id)
+    self.winner_id = player_id
+  end
+
+  def winner
+    player = Player.find_by(self.winner_id)
+  end
+
+  def calculate_loser
+    loser = self.players.select { |player| player.id != self.winner_id }
+    self.loser_id = loser[0].id
+  end
 
   def self.round(round_name)
     Match.where(name: "#{round_name}")
